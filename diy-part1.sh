20baf2a0e63da294767890e7c4c31c87d11da8d8
mylucien <53183558+mylucien@users.noreply.github.com>
2022-06-15 10:22:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' > feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo 'src-git packages https://github.com/Lienol/openwrt-packages.git;19.07' >> feeds.conf.default
+echo 'src-link luci ../extra/luci' >> feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git;openwrt-19.07' >> feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git;openwrt-19.07' >> feeds.conf.default
+echo 'src-git freifunk https://github.com/freifunk/openwrt-packages.git;openwrt-19.07' >> feeds.conf.default
+
+# other-plugins
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/n2n_v2 package/lean/n2n_v2
+svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-n2n_v2 extra/luci/applications/luci-app-n2n_v2
